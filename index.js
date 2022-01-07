cc23ce124367204ab75e3328f361ded1cdfe1b18
mauilema <srjorgemaciel2002@gmail.com>
2022-01-06 23:59:19 -0500

    updated chat functionality and css

--- index.js
+++ index.js
@@ -3,24 +3,71 @@ const app = express()
 const morgan = require('morgan')
 const server = require ('http').createServer(app)
 const io = require('socket.io')(server, { cors: { origin: "*"}})
-const home = require('./views/home.js')
+// const home = require('./views/home.js')
+const path = require('path')
+const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
 
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.send(home)
-}) 
+// app.get('/', (req, res) => {
+//     res.send(home)
+// }) 
 
-server.listen(3001, () => {
-    console.log('Server has started...')
-})
+const botName = 'NightBot'
 
-io.on('connection', (socket) => {
-    console.log('User connected: ', socket.id)
+//runs when client connects
+io.on('connection', socket => {
+    // console.log('User connected: ', socket.id)
 
-    socket.on("message", (data) => {
-        socket.broadcast.emit("message", data)
+    // socket.on("message", (data) => {
+    //     socket.broadcast.emit("message", data)
+    // })
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room)
+
+        socket.join(user.room)
+        //welcomes current user
+        socket.emit('message', formatMessage(botName, 'Welcome to Chat'))
+
+        //broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`))
+
+        //send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    })
+
+    //listen for chatMessage
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id)
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
+    })
+
+    //happens when user disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id)
+
+        if(user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`))
+
+             //send users and room info
+            io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+            })            
+        }
+
+
     })
 
 
+})
+
+server.listen(3000, () => {
+    console.log('Server is running...')
 })
