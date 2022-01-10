43da9c87670ac32c6da397c09d91b76abd09221c
mauilema <srjorgemaciel2002@gmail.com>
2022-01-09 19:24:37 -0500

    added more questions to the trivia game

--- public/js/questions.js
+++ public/js/questions.js
@@ -69,4 +69,159 @@ let questions = [
       "None of the above"
     ]
   },
+  {
+    numb: 7,
+    question: "How long did Brendan Eich take to write the JavaScript programming language?",
+    answer: "10 days",
+    options: [
+      "5 weeks",
+      "2 months",
+      "10 months",
+      "10 days"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following is not a reserved word in JavaScript?",
+    answer: "undefined",
+    options: [
+      "undefined",
+      "finally",
+      "throw",
+      "default"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following is the correct syntax to display “Fullstack Academy” in an alert box using JavaScript?",
+    answer: "alert('Fullstack Academy')",
+    options: [
+      "msg('Fullstack Academy')",
+      "alertbox('Fullstack Academy')",
+      "alert('Fullstack Academy')",
+      "msgbox('Fullstack Academy')"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the correct syntax for adding comments in JavaScript?",
+    answer: "//This is a comment",
+    options: [
+      "**This is a comment**",
+      "//This is a comment",
+      "#This is a comment#",
+      "$This is a comment$"
+    ]
+  },
+  {
+    numb: 11,
+    question: "What is the JavaScript syntax for printing values in Console?",
+    answer: "console.log(5)",
+    options: [
+      "console.print(5)",
+      "print.console(5)",
+      "print(5)",
+      "console.log(5)"
+    ]
+  },
+  {
+    numb: 12,
+    question: "What is the method in JavaScript used to remove the whitespace at the beginning and end of any string?",
+    answer: "trim()",
+    options: [
+      "strip() ",
+      "trim()",
+      "stripped()",
+      "trimmed()"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Which was the first browser to support JavaScript?",
+    answer: "Netscape",
+    options: [
+      "Mozilla Firefox",
+      "Google Chrome",
+      "Netscape",
+      "IE"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which of the following is not the JavaScript operator?",
+    answer: "this",
+    options: [
+      "this",
+      "typeof",
+      "delete",
+      "new"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which of the following is not a mouse event?",
+    answer: "onmousescroller",
+    options: [
+      "onclick",
+      "onmouseover",
+      "onmousemove",
+      "onmousescroller"
+    ]
+  },
+  {
+    numb: 16,
+    question: "What was JavaScript's original name?",
+    answer: "Mocha",
+    options: [
+      "Mocha",
+      "LiveScript",
+      "Escript",
+      "Javascript"
+    ]
+  },
+  {
+    numb: 17,
+    question: "What are the types of Pop up boxes available in JavaScript?",
+    answer: "All of the above",
+    options: [
+      "Prompt",
+      "Alert",
+      "Confirm",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Which one is Ternary Operator",
+    answer: "?:",
+    options: [
+      ">?",
+      "?#",
+      "?*",
+      "?:"
+    ]
+  },
+  {
+    numb: 19,
+    question: "How do you round the number 1.25 to the nearest integer?",
+    answer: "Math.round(1.25)",
+    options: [
+      "round(1.25)",
+      "Math.round(1.25)",
+      "Math.floor(1.25)",
+      "round(1.25)"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Which event is specific to the keyboard",
+    answer: "onkeydown",
+    options: [
+      "onclick",
+      "onfocus",
+      "onkeyboardpress",
+      "onkeydown"
+    ]
+  },
 ];
+
